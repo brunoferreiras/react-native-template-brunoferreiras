f143b3c6e0a0c8a44fd17a5835bc94fd63eaab30
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-11-07 15:50:03 -0300

    Fix imports and dependencies

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import configureStore from './src/store/configureStore';
 import { Provider } from 'react-redux';
+import AppRouter from './src/AppRouter';
+import configureStore from './src/store/configureStore';
 
 const store = configureStore();
 
@@ -9,24 +9,8 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>HelloWorld Template!</Text>
-        </View>
+        <AppRouter />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
